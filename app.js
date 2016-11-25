15a8a6fd9dbd6e334630863cb36c441f818164e4
stephenclark <stephen.k.clark@gmail.com>
2016-11-25 11:52:01 +1100

    lecture 9

--- app.js
+++ app.js
@@ -1,6 +1,27 @@
 var express = require('express');
+var path = require('path');
 var app = express();
 app.set('port', 3000);
+
+app.use(express.static(path.join(__dirname, 'public')))
+
+
+app.get('/json',function(req, res){
+    console.log("Get the json");
+    res
+        .status(200)
+        .json({"jsonData": true});
+});
+
+app.get('/file',function(req, res){
+    console.log("Get the file");
+    res
+        .status(200)
+        .sendFile(path.join(__dirname, 'app.js'));
+});
+
+
+
 
 var server = app.listen(app.get('port'), function(){
   var port = server.address().port;
